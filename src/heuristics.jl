a47f9fd3b99eceb2538c0e986d980e1591c26b51
Apostolos Chalkis <apostolos.chalkis@quantagonia.com>
2022-09-30 18:26:41 +0300

    remove Highs from best_solution()

--- src/heuristics.jl
+++ src/heuristics.jl
@@ -25,21 +25,3 @@ function find_best_solution(f::Function, o::SCIP.Optimizer, vars::Vector{MOI.Var
     @assert isfinite(best_val)
     return (best_v, best_val)
 end
-
-function find_best_solution_2(f::Function, o::HiGHS.Optimizer, vars::Vector{MOI.VariableIndex})
-    println("use HiGHS")
-    sols_vec =
-        unsafe_wrap(Vector{Ptr{Cvoid}}, SCIP.LibSCIP.SCIPgetSols(o), SCIP.LibSCIP.SCIPgetNSols(o))
-    best_val = Inf
-    best_v = nothing
-    for sol in sols_vec
-        v = SCIP.sol_values(o, vars, sol)
-        val = f(v)
-        if val < best_val
-            best_val = val
-            best_v = v
-        end
-    end
-    @assert isfinite(best_val)
-    return (best_v, best_val)
-end
