19d933ef6774c9a1ee01884fe49fa812895e1d26
Apostolos Chalkis <apostolos.chalkis@quantagonia.com>
2022-10-05 00:49:09 +0300

    use Highs

--- src/heuristics.jl
+++ src/heuristics.jl
@@ -3,7 +3,6 @@
 Finds the best solution in the SCIP solution storage, based on the objective function `f`.
 Returns the solution vector and the corresponding best value.
 """
-using HiGHS
 
 function find_best_solution2(f::Function, o::SCIP.Optimizer, vars::Vector{MOI.VariableIndex})
     println("vars: ")
@@ -38,7 +37,7 @@ function find_best_solution(f::Function, o, vars::Vector{MOI.VariableIndex})
     col_dual = Vector{Float64}(undef, 6)
     row_value = Vector{Float64}(undef, 6)
     row_dual = Vector{Float64}(undef, 6)
-    Highs_getSolution(o, col_value, col_dual, row_value, row_dual)
+    HiGHS.Highs_getSolution(o, col_value, col_dual, row_value, row_dual)
     println(col_value)
     println(col_dual)
     println(row_value)
