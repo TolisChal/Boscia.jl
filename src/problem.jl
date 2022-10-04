bcdfdd8b786db8e8d59f10b5a5d4c4af1cea7991
Apostolos Chalkis <apostolos.chalkis@quantagonia.com>
2022-10-05 01:42:28 +0300

    define global tol

--- src/problem.jl
+++ src/problem.jl
@@ -134,8 +134,9 @@ function is_linear_feasible_subroutine(o::MOI.ModelLike, ::Type{F}, ::Type{S}, v
         set = MOI.get(o, MOI.ConstraintSet(), c_idx)
         @debug("Constraint: $(F)-$(S) $(func) = $(val) in $(set)")
         dist = MOD.distance_to_set(MOD.DefaultDistance(), val, set)
-        scip_tol = MOI.get(o, MOI.RawOptimizerAttribute("numerics/feastol"))
-        if dist > 20.0 * scip_tol
+        #scip_tol = MOI.get(o, MOI.RawOptimizerAttribute("numerics/feastol"))
+        tol = 1.0e-06
+        if dist > 20.0 * tol
             return false
         end
     end
