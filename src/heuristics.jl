a433a6468adb8d00be5d90b5fab74f487dc0d89e
Apostolos Chalkis <apostolos.chalkis@quantagonia.com>
2022-10-05 01:11:45 +0300

    use Highs

--- src/heuristics.jl
+++ src/heuristics.jl
@@ -6,27 +6,28 @@ Returns the solution vector and the corresponding best value.
 #import HiGHS
 
 function find_best_solution2(f::Function, o::SCIP.Optimizer, vars::Vector{MOI.VariableIndex})
-    println("vars: ")
-    for var in vars
-        println(var)
-    end
+    #println("vars: ")
+    #for var in vars
+    #    #println(var)
+    #end
     sols_vec =
         unsafe_wrap(Vector{Ptr{Cvoid}}, SCIP.LibSCIP.SCIPgetSols(o), SCIP.LibSCIP.SCIPgetNSols(o))
     #println(sols_vec)
     best_val = Inf
     best_v = nothing
-    println("sols: ")
+    #println("sols: ")
     for sol in sols_vec
-        println(sol)
+        #println(sol)
         v = SCIP.sol_values(o, vars, sol)
-        println(v)
+        #println(v)
         val = f(v)
-        println(val)
+        #println(val)
         if val < best_val
             best_val = val
             best_v = v
         end
     end
+    println(best_v)
     println("---------------------------\n")
     @assert isfinite(best_val)
     return (best_v, best_val)
@@ -40,14 +41,17 @@ function find_best_solution(f::Function, o::HiGHS.Optimizer, vars::Vector{MOI.Va
     row_dual = Vector{Float64}(undef, 6)
     HiGHS.Highs_getSolution(o, col_value, col_dual, row_value, row_dual)
     println(col_value)
-    println(col_dual)
-    println(row_value)
-    println(row_dual)
+    #println(col_dual)
+    #println(row_value)
+    #println(row_dual)
+    best_val = f(col_value)
+    best_v = col_value
+    println(best_val)
     #sols_vec =
     #    unsafe_wrap(Vector{Ptr{Cvoid}}, SCIP.LibSCIP.SCIPgetSols(o), SCIP.LibSCIP.SCIPgetNSols(o))
     #println(sols_vec)
-    best_val = 7.5
-    best_v = nothing
+    #best_val = 7.5
+    #best_v = nothing
     #println("sols: ")
     #for sol in sols_vec
     #    println(sol)
