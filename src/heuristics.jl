75e37cd451f34401cc12401ade7037432bf0ef03
Apostolos Chalkis <apostolos.chalkis@quantagonia.com>
2022-10-05 01:22:31 +0300

    use Highs

--- src/heuristics.jl
+++ src/heuristics.jl
@@ -35,38 +35,18 @@ function find_best_solution(f::Function, o::SCIP.Optimizer, vars::Vector{MOI.Var
 end
 
 function find_best_solution(f::Function, o::HiGHS.Optimizer, vars::Vector{MOI.VariableIndex})
-    println(Highs_getNumCol(o))
-    println(Highs_getNumRow(o))
-    col_value = Vector{Float64}(undef, 6)
-    col_dual = Vector{Float64}(undef, 6)
-    row_value = Vector{Float64}(undef, 6)
-    row_dual = Vector{Float64}(undef, 6)
+    ncol = Highs_getNumCol(o)
+    nrow = Highs_getNumRow(o)
+    col_value = Vector{Float64}(undef, ncol)
+    col_dual = Vector{Float64}(undef, ncol)
+    row_value = Vector{Float64}(undef, nrow)
+    row_dual = Vector{Float64}(undef, nrow)
     HiGHS.Highs_getSolution(o, col_value, col_dual, row_value, row_dual)
     println(col_value)
-    #println(col_dual)
-    #println(row_value)
-    #println(row_dual)
-    best_val = f(col_value)
+    val = f(col_value)
     best_v = col_value
     println(best_val)
-    #sols_vec =
-    #    unsafe_wrap(Vector{Ptr{Cvoid}}, SCIP.LibSCIP.SCIPgetSols(o), SCIP.LibSCIP.SCIPgetNSols(o))
-    #println(sols_vec)
-    #best_val = 7.5
-    #best_v = nothing
-    #println("sols: ")
-    #for sol in sols_vec
-    #    println(sol)
-    #    v = SCIP.sol_values(o, vars, sol)
-    #    println(v)
-    #    val = f(v)
-    #    println(val)
-    #    if val < best_val
-    #        best_val = val
-    #        best_v = v
-    #    end
-    #end
     println("------------HiGHS---------------\n")
-    #@assert isfinite(best_val)
-    return (best_v, best_val)
+    @assert isfinite(best_val)
+    return (col_value, val)
 end
